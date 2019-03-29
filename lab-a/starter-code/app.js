88d89c276df987910e3180f5c14c3d060309246c
STEPHEN CHU <stephen.chu530@gmail.com>
2019-03-29 15:48:10 -0700

    feat: created function sumArray

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -79,12 +79,13 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let totalSum = sumAndMultiply(sumArr[0], sumArr[1], sumArr[2])[0];
+  return [totalSum, `${sumArr} was passed in as an array of numbers, and ${totalSum} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
